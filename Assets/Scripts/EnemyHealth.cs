d981197c1d86367751cf5313577aeb4ac97f1c9e
MrOwl715 <trannguyendinh.yhd@gmail.com>
2026-02-04 16:03:07 +0700

    DEMO Part 4 completed

--- Assets/Scripts/EnemyHealth.cs
+++ Assets/Scripts/EnemyHealth.cs
@@ -1,32 +1,10 @@
 ﻿using UnityEngine;
 
-public class EnemyHealth : MonoBehaviour
+public class EnemyHealth : Health
 {
-    [Header("Kéo Prefab vụ nổ vào đây")]
-    public GameObject explosionPrefab;
-
-    // Hàm này chạy khi có vật thể (có Is Trigger) bay xuyên qua
-    private void OnTriggerEnter2D(Collider2D collision)
+    protected override void Die()
     {
-        // [PRO TIP] Kiểm tra xem cái chạm vào có phải là Đạn hay không?
-        // Nếu muốn kỹ tính hơn thì dùng: if(collision.CompareTag("Bullet"))
-        Debug.Log("Đã chạm vào: " + collision.name);
-        // 1. Xóa viên đạn vừa chạm vào mình (để nó không bay xuyên qua)
-        Destroy(collision.gameObject);
-
-        // 2. Kích hoạt cái chết
-        Die();
-    }
-
-    private void Die()
-    {
-        // Tạo vụ nổ tại chỗ chết
-        if (explosionPrefab != null)
-        {
-            Instantiate(explosionPrefab, transform.position, transform.rotation);
-        }
-
-        // Xóa phi thuyền địch
-        Destroy(gameObject);
+        base.Die();
+        Debug.Log("Enemy died");
     }
 }
--- Assets/Space Shooter Template FREE/Scripts/Bullet.cs
+++ Assets/Space Shooter Template FREE/Scripts/Bullet.cs
@@ -2,19 +2,27 @@
 
 public class Bullet : MonoBehaviour
 {
-    // Tốc độ bay của đạn (trong PDF là flySpeed [cite: 51])
+    public int damage;
     public float speed = 10f;
 
     void Update()
     {
-        // Di chuyển đạn lên trên theo trục Y (Vector3.up tương đương với new Vector3(0, 1, 0))
-        // Time.deltaTime giúp game mượt trên mọi FPS
-        transform.Translate(Vector3.up * speed * Time.deltaTime);
 
-        // [PRO TIP] Tự hủy đạn nếu bay quá xa để đỡ tốn ram (Giáo trình chưa dạy nhưng nên làm)
+
+        transform.Translate(Vector3.up * speed * Time.deltaTime);
         if (transform.position.y > 10f)
         {
             Destroy(gameObject);
         }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        var enemy = collision.GetComponent<EnemyHealth>();
+        if (enemy != null)
+        {
+            enemy.TakeDamage(damage);
+        }
+        Destroy(gameObject);
     }
 }
